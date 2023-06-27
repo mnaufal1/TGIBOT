42b8668b80fdcd10dbe02485200dc9b3fb719378
Muhammad Alif Naufal <77015662+mnaufal1@users.noreply.github.com>
2023-06-27 12:42:27 +0300

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,17 @@
+#Code that allows the Discord bot to run online in Replit
+
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Hello. I am alive!"
+
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
